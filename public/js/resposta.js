0322d9fa13c696a8146a0d5280b86b74c5f77916
Diego William Fernandes de Carvalho <69658917+diegowfc@users.noreply.github.com>
2022-01-01 23:49:31 -0300

    Update resposta.js

--- public/js/resposta.js
+++ public/js/resposta.js
@@ -8,6 +8,7 @@ function comparaResposta(data) {
     var respostaUsuario = campo.val();
     if (contador <= 10) var respostaPergunta = data[contador].resposta;
     if (respostaUsuario == respostaPergunta) {
+        campo.addClass("campo-digitacao-acerto");
         contador++;
         contaAcertos(contador);
         reiniciaCampo();
@@ -17,6 +18,9 @@ function comparaResposta(data) {
             reiniciaCampo();
         }
     }
+    setTimeout(function() {
+        campo.removeClass("campo-digitacao-acerto");
+    }, 500);
 }
 
 function mostraFrase() {
