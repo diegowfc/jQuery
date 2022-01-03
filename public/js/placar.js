89339a3b89d322fafee5aeaf3266d35549aa927e
Diego William Fernandes de Carvalho <69658917+diegowfc@users.noreply.github.com>
2022-01-02 23:32:02 -0300

    Adicionando funcionalidade que salva no servidor

--- public/js/placar.js
+++ public/js/placar.js
@@ -22,6 +22,39 @@ function inserePlacar() {
     placar.slideDown(500);
 }
 
+function salvaPlacar() {
+    var placar = [];
+    var linhas = $("tbody>tr");
+
+    linhas.each(function() {
+        var jogador = $(this).find("td:nth-child(1)").text();
+        var score = $(this).find("td:nth-child(2)").text();
+
+        var lista = {
+            jogador: jogador,
+            pontos: score
+        };
+
+        placar.push(lista);
+    });
+
+    var dados = {
+        placar: placar
+    }
+    $.post("http://localhost:3000/placar", dados, function() {
+        console.log("salvou no servidor");
+    })
+}
+
+function atualizaPlacar() {
+    $.get("http://localhost:3000/placar", function(data) {
+        $(data).each(function() {
+            var linha = novoPlacar(this.jogador, this.pontos);
+            $("tbody").append(linha);
+        })
+    });
+}
+
 function exibePlacar() {
     var botao = $("#botao-placar");
     botao.on("click", function() {
